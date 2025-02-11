449ed01b2dd9ef8f0c1a3e1f8351dab44053c2a5
bbye98 <benjaminbobinye@gmail.com>
2025-02-10 22:09:20 -0500

    Continue work on CudaCalcDPDForceKernel

--- extra/openmm-dpd/platforms/cuda/src/CudaCalcDPDForceKernel.cpp
+++ extra/openmm-dpd/platforms/cuda/src/CudaCalcDPDForceKernel.cpp
@@ -10,6 +10,21 @@ void OpenMM::CudaCalcDPDForceKernel::initialize(const System& system,
                          &system.getForce(forceIndex) != &force;
          ++forceIndex);
     std::string prefix = "dpd" + cu.intToString(forceIndex) + "_";
+
+    std::vector<std::pair<int, int>> excludedPairs;
+    std::vector<int> exceptionIndices;
+    for (int i = 0; i < force.getNumExceptions(); i++) {
+        int particle1, particle2;
+        double A, gamma, rCut;
+        force.getExceptionParameters(i, particle1, particle2, A, gamma, rCut);
+        excludedPairs.emplace_back(std::make_pair(particle1, particle2));
+        if (A != 0) {
+            exceptionIndices.push_back(i);
+        }
+    }
+
+    int numParticles = force.getNumParticles();
+    // TODO
 }
 
 void OpenMM::CudaCalcDPDForceKernel::copyParametersToContext(
